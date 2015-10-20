3f66b1370cc52a0cafa1802fc187aad7bc1a2147
Matt Parsons <parsonsmatt@gmail.com>
2015-10-20 12:38:28 -0400

    more API fns

--- src/QuickLift/Api.elm
+++ src/QuickLift/Api.elm
@@ -1,25 +1,60 @@
 module QuickLift.Api where
 
 import Effects exposing (Effects, Never)
+import Date exposing (Date)
+import Date
 import Http
 import Json.Decode as Json
+import Json.Decode exposing ((:=))
 import Task
 
-getUser : Effects Action
-getUser =
-  Http.get decodeUser "user/1" 
+getUser : Int -> Effects Action
+getUser i =
+  Http.get decodeUser ("users/" ++ toString i)
     |> Task.toMaybe
     |> Task.map GetUser
     |> Effects.task
 
+getUsers : Effects Action
+getUsers =
+  Http.get decodeUsers "users"
+    |> Task.toMaybe
+    |> Task.map GetUsers
+    |> Effects.task
+
+getSession : Effects Action
+getSession =
+  Http.get decodeSession "sessions"
+    |> Task.toMaybe
+    |> Task.map GetSession
+    |> Effects.task
+
 type Action
   = GetUser (Maybe User)
+  | GetUsers (Maybe (List User))
+  | GetSession (Maybe Session)
 
 type User = User
   { name : String
   , email : String
   }
 
+mkUser n e = User { name = n, email = e }
+
+type Session = Session
+  { text : String
+  , date : String
+  }
+
+mkSession t d = Session { text = t, date = d }
+
 decodeUser : Json.Decoder User
 decodeUser =
-  User <| Json.at ["name"] Json.string <| Json.at ["email"] Json.string
+  Json.object2 mkUser ("name" := Json.string) ("email" := Json.string)
+
+decodeUsers : Json.Decoder (List User)
+decodeUsers = Json.list decodeUser
+
+decodeSession : Json.Decoder Session
+decodeSession =
+  Json.object2 mkSession ("text" := Json.string) ("date" := Json.string) 
